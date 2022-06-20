de56ab0f13cea368ef44fe0916c789d8e5687041
Tạ Quang Khôi <khoi0941@gmail.com>
2022-06-20 19:19:00 +0700

    Try final variable

--- Pham-Thi-Thuy-Hang/yeu_em/La_Toi.java
+++ Pham-Thi-Thuy-Hang/yeu_em/La_Toi.java
@@ -3,5 +3,7 @@ package yeu_em;
 public class La_Toi {
 	public static void main (String[] args) {
 		System.out.println("Cho em mai, nu cuoi no tren moi");
+		boolean EmYeuToi = false;
+		final boolean ToiTiepTuc = true;
 	}
 }
