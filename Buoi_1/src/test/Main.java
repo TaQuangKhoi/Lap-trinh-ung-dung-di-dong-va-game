7ed51777d81704e81b470b9116a5a9ad6bc7cb1d
Tạ Quang Khôi <khoi0941@gmail.com>
2022-06-20 14:02:56 +0700

    Test to fix error
    Error: Could not find or load main class test.Main
    Caused by: java.lang.ClassNotFoundException: test.Main

--- Buoi_1/src/test/Main.java
+++ Buoi_1/src/test/Main.java
@@ -1,14 +1,14 @@
+package test;
 
-
-public class Hello {
+public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello World");
 		byte a = 3;     
         byte b = 8;     
         byte result;
-        result = (byte) (a + b); //addition typecasted
+        result = (byte) (a + b);
 
         System.out.println(result);
 	}
