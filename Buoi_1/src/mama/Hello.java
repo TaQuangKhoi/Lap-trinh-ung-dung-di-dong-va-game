51f37d0ae87d315ad6177442da209d693ca78c58
Tạ Quang Khôi <khoi0941@gmail.com>
2022-06-20 14:10:04 +0700

    Tạo lại project nhưng vẫn bị lỗi

---
+++ Buoi_1/src/mama/Hello.java
@@ -0,0 +1,16 @@
+package mama;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		byte a = 3;     
+        byte b = 8;     
+        byte result;
+        result = (byte) (a + b);
+
+        System.out.println(result);
+	}
+
+}
