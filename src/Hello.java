2c8bb567617824e303eb7addc7981204a3edd201
Tạ Quang Khôi <khoi0941@gmail.com>
2022-06-20 11:28:45 +0700

    Tạo project đàu tiên trên Java môn này

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		byte a = 3;     
+        byte b = 8;     
+        byte result;
+        result = (byte) (a + b); //addition typecasted
+
+        System.out.println(result);
+	}
+
+}
