a070c8a03fc34499c8245b4c74298c848a4fe0a2
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2022-06-21 03:19:50 +0000

    create project while

---
+++ Buoi_2/while/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int tong = 0; i =1;
+        do{
+            tong +=i;i+=2;
+        }
+        while (i<=20);
+        System.out.println(tong);
+    }
+}
