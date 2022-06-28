3f2e454fbf84b8ca44d1baff7cfbd6eda32fbdf8
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2022-06-28 01:21:05 +0000

    Create TestInput.java

--- Buoi_3/Testing/Test.java
+++ Buoi_3/Testing/Test.java
@@ -1,16 +1,8 @@
 public class Test{
     public static void main (String [] a) {
-        int n = 12, k = 2;
-        while(k<n){
-            for (k = 2; k <=n; k++){
-                if(n%k==0){
-                    System.out.print(k + " ");
-                    n = n/k;
-                    break;
-                }
-                // Khi n%k != 0 => k++ và không set k=2 lần nữa
-            }
-        }
+        String s = "Mot Hai Ba Bon";
+        String s2 = s.substring(4, 7);
+        System.out.println(s2);
     }
     
 }
