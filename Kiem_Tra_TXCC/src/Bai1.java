73398c4d2027505498a0a2619540164836b02b5b
Tạ Quang Khôi <khoi0941@gmail.com>
2022-07-04 09:09:14 +0700

    Bắt đầu kiểm tra

---
+++ Kiem_Tra_TXCC/src/Bai1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Bai1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String bien_nhap_vao = sc.nextLine();
+		sc.close();
+	}
+
+}
--- TestNew/src/TestNe.java
+++ TestNew/src/TestNe.java
@@ -1,9 +1,25 @@
 
 public class TestNe {
-
+	private int number = 10;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Khôi");
+		
+		TestNe tn = new TestNe();
+		TestNe tn2 = tn;
+		tn2.Them();
+		tn.Them();
+		
+		System.out.println(tn2.number);
+		System.out.print(tn.number);
 	}
-
+	public void Print() {
+		int number2 = 30;
+		System.out.print(number2);
+		this.number = 20;
+		System.out.print(this.number);
+	}
+	
+	public void Them() {
+		number += number + 1;
+	}
 }
