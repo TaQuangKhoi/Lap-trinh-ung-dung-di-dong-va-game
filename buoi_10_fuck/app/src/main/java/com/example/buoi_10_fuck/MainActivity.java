8567446a87a94db335f49e1565fba11ccb9eed31
Tạ Quang Khôi <khoi0941@gmail.com>
2022-07-19 09:54:49 +0700

    Học về Dialog

--- buoi_10_fuck/app/src/main/java/com/example/buoi_10_fuck/MainActivity.java
+++ buoi_10_fuck/app/src/main/java/com/example/buoi_10_fuck/MainActivity.java
@@ -1,26 +1,29 @@
 package com.example.buoi_10_fuck;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.buoi_10_fuck.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-    private TextView textView;
+    private TextView tv1;
+    private Button btn1;
+    private Button btn2;
+    private EditText et1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +46,55 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
-        textView = (TextView)findViewById(R.id.tv1); // Thêm cài này để ánh xạ:v - từ android 3., có thễ bỏ qua ép kiểu
 
+        // Ánh xạ
+        tv1 = (TextView)findViewById(R.id.tv1); // Thêm cài này để ánh xạ:v - từ android 3., có thễ bỏ qua ép kiểu
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        et1 = (EditText) findViewById(R.id.et1);
+        // Đăng ký Event
+        /* Cach 2
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn1.setText("123");
+            }
+        });*/
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        //tv1.setText("Cách 3");
+        /*
+        Toast.makeText(this,
+                et1.getText().toString(), Toast.LENGTH_SHORT).show();*/
+        AlertDialog.Builder al1 = new AlertDialog.Builder(this);
+        al1.setTitle("");
+        al1.setMessage("Thử nghiệm");
+        al1.setIcon(R.drawable.ic_plus);
+        al1.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Đã nhấn đồng ý", Toast.LENGTH_SHORT).show();
+            }
+        });
+        al1.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Đã nhấn không đồng ý", Toast.LENGTH_SHORT).show();
+            }
+        });
+        al1.setNeutralButton("Cancel", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Đã nhấn huỷ", Toast.LENGTH_SHORT).show();
+            }
+        });
+        al1.show();
     }
 
    /* @Override
@@ -54,7 +104,8 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 */
+    /*
     public void doitext(View view) {
-textView.setText("jadh");
-    }
+        textView.setText("jadh");
+    }*/
 }
