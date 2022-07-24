ba793713e025bd8fea7f275f312f39184065de00
Tạ Quang Khôi <khoi0941@gmail.com>
2022-07-24 23:49:29 +0700

    Create new project - relearn_buoi_10 - not use fragment..

---
+++ relearn_buoi_10/app/src/main/java/com/example/relearn_buoi_10/Cach_1.java
@@ -0,0 +1,19 @@
+package com.example.relearn_buoi_10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Cach_1 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cach1);
+
+    }
+}
---
+++ relearn_buoi_10/app/src/main/java/com/example/relearn_buoi_10/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.relearn_buoi_10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn1;
+    Button btn2;
+    Button btn3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btnc1);
+        btn2 = findViewById(R.id.btnc2);
+        btn3 = findViewById(R.id.btnc3);
+        Intent cach_1 = new Intent(this, Cach_1.class);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Cách 1", Toast.LENGTH_SHORT).show();
+                startActivity(cach_1);
+            }
+        });
+
+
+    }
+}
