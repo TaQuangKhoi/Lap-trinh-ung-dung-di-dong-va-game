30d12c2c894f98f8b334eb3d549989560f1b8482
Tạ Quang Khôi <khoi0941@gmail.com>
2022-07-25 08:41:57 +0700

    Thêm 1 EditText và 1 Button

--- buoi_11/app/src/main/java/com/example/buoi_11/MainActivity.java
+++ buoi_11/app/src/main/java/com/example/buoi_11/MainActivity.java
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     ListView lv1;
+    EditText edt1;
+    Button btn1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Ánh xạ
+        // Các ánh xạ
         lv1 = findViewById(R.id.lv1);
+        edt1 = findViewById(R.id.edt1);
 
         // Tạo dữ liệu
         //final String[] data = {"Android", "IOS", "Windows", "Linux", "Mac OS", "Ubuntu", "Debian"};
