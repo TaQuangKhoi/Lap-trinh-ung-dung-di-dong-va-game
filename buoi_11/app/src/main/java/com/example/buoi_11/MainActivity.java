cfa99dc88e23aec7841980c0ee12cba6eb120472
Tạ Quang Khôi <khoi0941@gmail.com>
2022-07-25 08:03:32 +0700

    Tạo prj cho Buổi 11
    - Thêm ListView, ánh xạ vào Java
    - tạo mảng String, dùng Adapter hứng data, gán vào ListView

---
+++ buoi_11/app/src/main/java/com/example/buoi_11/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.buoi_11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Ánh xạ
+        lv1 = findViewById(R.id.lv1);
+
+        // Tạo dữ liệu
+        final String[] data = {"Android", "IOS", "Windows", "Linux", "Mac OS", "Ubuntu", "Debian"};
+
+        // Tạo adapter
+        ArrayAdapter adapter_data_os = new ArrayAdapter(this, android.R.layout.simple_list_item_1, data);
+
+        // Gán adapter cho listview
+        lv1.setAdapter(adapter_data_os);
+    }
+}
