dbe16d9231980e42f4b07be63e53af3533530924
Tạ Quang Khôi <khoi0941@gmail.com>
2022-07-26 17:01:17 +0700

    Thêm các cmt giải thích các dòng code

--- buoi_12/app/src/main/java/com/example/buoi_12/BusesAdapter.java
+++ buoi_12/app/src/main/java/com/example/buoi_12/BusesAdapter.java
@@ -14,8 +14,8 @@ import androidx.annotation.Nullable;
 public class BusesAdapter extends ArrayAdapter<Buses> {
 
     // khai báo thuộc tính để có thể truy cập mọi nơi trong class
-    private Context context;
-    private int resource;
+    private Context context; // Trà về Activity mà ListView hiển thị
+    private int resource; // loại layout cần được sử dụng
 
     // Viết lại Constructor
     // Là method cần được ghi đè khi kế thừa ArrayAdapter
@@ -25,18 +25,20 @@ public class BusesAdapter extends ArrayAdapter<Buses> {
         this.resource = resource;
     }
 
+    // ghi đè getVIew để trả về CustomItemView của ListView
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View customView = LayoutInflater.from(context).inflate(resource, null);
 
-        // Ánh xạ
+        // Ánh xạ thông qua customView (chưa có data)
         ImageView imageView = customView.findViewById(R.id.ivBus);
         TextView tvNameBus = customView.findViewById(R.id.tvNameBus);
         TextView tvRouteBus = customView.findViewById(R.id.tvRouteBus);
 
-        // Lấy dữ liệu từ từng phần tử trong mảng
-        Buses bus = getItem(position);
+        Buses bus = getItem(position); // Lấy đối tượng
+
+        // Lấy dữ liệu từ phần tử với position trong mảng đưa vào từng View
         imageView.setImageResource(getItem(position).getIdImage());
         tvNameBus.setText(getItem(position).getNameBus());
         tvRouteBus.setText(getItem(position).getRouteBus());
