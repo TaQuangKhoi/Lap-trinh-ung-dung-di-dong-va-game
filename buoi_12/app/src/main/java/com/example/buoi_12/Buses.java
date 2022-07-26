b185fc1468c4b030972befe63655f2a4d84bcb7c
Tạ Quang Khôi <khoi0941@gmail.com>
2022-07-26 08:39:03 +0700

    Auto generate setter and getter

--- buoi_12/app/src/main/java/com/example/buoi_12/Buses.java
+++ buoi_12/app/src/main/java/com/example/buoi_12/Buses.java
@@ -1,7 +1,35 @@
 package com.example.buoi_12;
 
 public class Buses {
+    // Khai báo thuộc tính
     private int idImage;
     private String nameBus, routeBus;
+
+    // Tạo các getter và setter cho các thuộc tính trên
+    public int getIdImage() {
+        return idImage;
+    }
+
+    public void setIdImage(int idImage) {
+        this.idImage = idImage;
+    }
+
+    public String getNameBus() {
+        return nameBus;
+    }
+
+    public void setNameBus(String nameBus) {
+        this.nameBus = nameBus;
+    }
+
+    public String getRouteBus() {
+        return routeBus;
+    }
+
+    public void setRouteBus(String routeBus) {
+        this.routeBus = routeBus;
+    }
+
+
 }
 
