cfbfd2d1ef8d779b36b553b85657b75233300752
Tạ Quang Khôi <khoi0941@gmail.com>
2022-08-01 07:55:51 +0700

    Ánh xạ và chuẩn bị Listener

---
+++ kt_thuchanh_txcc/app/src/main/java/com/example/kt_thuchanh_txcc/grid_view.java
@@ -0,0 +1,14 @@
+package com.example.kt_thuchanh_txcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class grid_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_view);
+    }
+}
