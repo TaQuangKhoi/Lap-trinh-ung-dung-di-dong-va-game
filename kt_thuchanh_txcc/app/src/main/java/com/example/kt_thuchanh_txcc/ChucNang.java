1d5c0beaf2dbe8ff981771d3c389b013ef3730e6
Tạ Quang Khôi <khoi0941@gmail.com>
2022-08-01 08:25:00 +0700

    Thêm thuộc tính Color

--- kt_thuchanh_txcc/app/src/main/java/com/example/kt_thuchanh_txcc/ChucNang.java
+++ kt_thuchanh_txcc/app/src/main/java/com/example/kt_thuchanh_txcc/ChucNang.java
@@ -1,20 +1,24 @@
 package com.example.kt_thuchanh_txcc;
 
+import android.graphics.Color;
+
 public class ChucNang {
-    int imID;
+    int imIconID;
     String tenChucNang;
+    Color color;
 
-    public ChucNang(int imID, String tenChucNang) {
-        this.imID = imID;
+    public ChucNang(int imID, String tenChucNang, Color color) {
+        this.imIconID = imID;
         this.tenChucNang = tenChucNang;
+        this.color = color;
     }
 
-    public int getImID() {
-        return imID;
+    public int getImIconID() {
+        return imIconID;
     }
 
-    public void setImID(int imID) {
-        this.imID = imID;
+    public void setImIconID(int imIconID) {
+        this.imIconID = imIconID;
     }
 
     public String getTenChucNang() {
@@ -23,5 +27,13 @@ public class ChucNang {
 
     public void setTenChucNang(String tenChucNang) {
         this.tenChucNang = tenChucNang;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
     }
 }
