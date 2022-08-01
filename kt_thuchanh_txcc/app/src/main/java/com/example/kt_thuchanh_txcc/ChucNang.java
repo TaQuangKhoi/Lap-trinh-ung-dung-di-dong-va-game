b39b9e7b64e6242054a93a2b26df27d46d9d99ae
Tạ Quang Khôi <khoi0941@gmail.com>
2022-08-01 08:10:13 +0700

    Viết xong lớp ChucNang

--- kt_thuchanh_txcc/app/src/main/java/com/example/kt_thuchanh_txcc/ChucNang.java
+++ kt_thuchanh_txcc/app/src/main/java/com/example/kt_thuchanh_txcc/ChucNang.java
@@ -1,5 +1,27 @@
 package com.example.kt_thuchanh_txcc;
 
 public class ChucNang {
+    int imID;
+    String tenChucNang;
 
+    public ChucNang(int imID, String tenChucNang) {
+        this.imID = imID;
+        this.tenChucNang = tenChucNang;
+    }
+
+    public int getImID() {
+        return imID;
+    }
+
+    public void setImID(int imID) {
+        this.imID = imID;
+    }
+
+    public String getTenChucNang() {
+        return tenChucNang;
+    }
+
+    public void setTenChucNang(String tenChucNang) {
+        this.tenChucNang = tenChucNang;
+    }
 }
