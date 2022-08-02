ce3485fd9ad1c7f8a323769e3bc487f5eb91d2d2
Tạ Quang Khôi <khoi0941@gmail.com>
2022-08-02 08:37:56 +0700

    tạo act mới

---
+++ buoi_14_intent/app/src/main/java/com/example/buoi_14_intent/show_data_serializable.java
@@ -0,0 +1,14 @@
+package com.example.buoi_14_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class show_data_serializable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_data_serializable);
+    }
+}
