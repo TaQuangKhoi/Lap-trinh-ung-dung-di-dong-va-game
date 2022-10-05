1d0a39c6dfce3c0773282ff0759d0b897ac159f4
Tạ Quang Khôi <khoi0941@gmail.com>
2022-10-05 08:11:50 +0700

    Viết xong hàm createNotificationChannel

--- Phan-2/NotificationLesson/app/src/main/java/com/taquangkhoi/notificationlesson/Notification.java
+++ Phan-2/NotificationLesson/app/src/main/java/com/taquangkhoi/notificationlesson/Notification.java
@@ -1,8 +1,13 @@
 package com.taquangkhoi.notificationlesson;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 
 public class Notification extends Application {
+    public static final String CHANNEL_ID = "CHANNEL_1";
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -11,6 +16,18 @@ public class Notification extends Application {
     }
 
     private void createNotificationChannel() {
+        // Phải dùng API Level 26 trở lên
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
 }
