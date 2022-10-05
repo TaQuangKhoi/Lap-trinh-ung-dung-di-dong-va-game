308b6fb254eeb8cefb5be9f67c5bfb0801ca45e6
Tạ Quang Khôi <khoi0941@gmail.com>
2022-10-05 08:17:34 +0700

    Ánh xạ, và thêm Event

--- Phan-2/NotificationLesson/app/src/main/java/com/taquangkhoi/notificationlesson/MainActivity.java
+++ Phan-2/NotificationLesson/app/src/main/java/com/taquangkhoi/notificationlesson/MainActivity.java
@@ -3,12 +3,26 @@ package com.taquangkhoi.notificationlesson;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_send_notification;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_send_notification = findViewById(R.id.btn_send_notification);
+        btn_send_notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                senNotification();
+            }
+        });
+    }
+
+    private void senNotification() {
+
     }
 }
