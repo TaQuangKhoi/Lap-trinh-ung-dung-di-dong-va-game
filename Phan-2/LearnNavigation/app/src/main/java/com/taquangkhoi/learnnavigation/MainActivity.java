707778852becf97f31b8e6f0bfa834862a0b2bd2
Tạ Quang Khôi <khoi0941@gmail.com>
2022-10-12 09:38:26 +0700

    Viết xong Main Activity .java

--- Phan-2/LearnNavigation/app/src/main/java/com/taquangkhoi/learnnavigation/MainActivity.java
+++ Phan-2/LearnNavigation/app/src/main/java/com/taquangkhoi/learnnavigation/MainActivity.java
@@ -1,14 +1,74 @@
 package com.taquangkhoi.learnnavigation;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.widget.Toolbar; // dùng cái mới này thay vì android.widget.Toolbar
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar(toolbar);
+
+        addControls();
+    }
+
+    private void addControls() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.toolbar);
+
+        ActionBarDrawerToggle actionBarDrawerToggle =
+                new ActionBarDrawerToggle(this,
+                        drawerLayout, toolbar,
+                        R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // do something
+        } else if (id == R.id.nav_home) {
+            // do something
+        } else if (id == R.id.nav_student) {
+            // do something
+        } else if (id == R.id.nav_history) {
+            // do something
+        } else if (id == R.id.nav_myprofile) {
+            // do something
+        } else if (id == R.id.nav_change_password) {
+            // do something
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
