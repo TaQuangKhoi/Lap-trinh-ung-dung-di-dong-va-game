afa2104c08be19f235178539c25597ae5bcc80af
Tạ Quang Khôi <khoi0941@gmail.com>
2022-10-26 09:42:43 +0700

    Tạo project học ChanegLanguages

--- Phan-2/LearnSharePreferencesWithQuynh/app/src/main/java/com/khoitien/learnsharepreferenceswithquynh/MainActivity.java
+++ Phan-2/LearnSharePreferencesWithQuynh/app/src/main/java/com/khoitien/learnsharepreferenceswithquynh/MainActivity.java
@@ -2,26 +2,86 @@ package com.khoitien.learnsharepreferenceswithquynh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edtHoTen;
     EditText edtMSV;
     CheckBox cbxSaveInfo;
-    CheckBox btnDangNhap;
+    Button btnDangNhap;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AddSharedPreferences();
 
         AddControls();
+        AddEvents();
+
+
+        ReloadSavedInfo();
     }
 
     private void AddControls() {
         edtHoTen = findViewById(R.id.edt_hovaten);
-        edtMSV = findViewById(R.id.edt_msv);
+        edtMSV = findViewById(R.id.edt_password);
+        cbxSaveInfo = findViewById(R.id.cbx_save_info);
+        btnDangNhap = findViewById(R.id.btn_dang_nhap);
+    }
+
+    private void AddEvents() {
+        btnDangNhap.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, ShowResult.class);
+            startActivity(intent);
+        });
+
+        cbxSaveInfo.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (cbxSaveInfo.isChecked()){
+                SaveInfo();
+                editor.putBoolean("key_bool", b);
+                editor.apply();
+                Log.i("SaveInfo", "Đã luu");
+            } else {
+                editor.clear();
+                editor.putBoolean("key_bool", b);
+                editor.apply();
+                Toast.makeText(MainActivity.this, "Đã xóa", Toast.LENGTH_SHORT);
+                Log.i("SaveInfo", "Đã xóa");
+            }
+        });
+    }
+
+    private void AddSharedPreferences() {
+        sharedPreferences = getSharedPreferences("quynh", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+    }
+
+    private void SaveInfo() {
+        editor.putString("key_name", edtHoTen.getText().toString()).apply();
+        editor.putString("key_password", edtMSV.getText().toString()).apply();
+        editor.commit();
+        Toast.makeText(MainActivity.this, "Đã lưu", Toast.LENGTH_SHORT);
+    }
+
+    private void ReloadSavedInfo() {
+        cbxSaveInfo.setChecked(sharedPreferences.getBoolean("key_bool", false));
+        if(sharedPreferences.getBoolean("key_bool", false)){
+            String name = sharedPreferences.getString("key_name", "Hoten");
+            String password = sharedPreferences.getString("key_password", "Password");
+            edtHoTen.setText(name);
+            edtMSV.setText(password);
+            Toast.makeText(MainActivity.this, "Đã khôi phục", Toast.LENGTH_SHORT);
+        }
     }
 }
--- Phan-2/LearnSharePreferencesWithQuynh/app/src/main/java/com/khoitien/learnsharepreferenceswithquynh/ShowResult.java
+++ Phan-2/LearnSharePreferencesWithQuynh/app/src/main/java/com/khoitien/learnsharepreferenceswithquynh/ShowResult.java
@@ -2,13 +2,24 @@ package com.khoitien.learnsharepreferenceswithquynh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShowResult extends AppCompatActivity {
+    TextView tvwHoTen;
+    TextView tvwMatKhau;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_result);
+
+        tvwHoTen = findViewById(R.id.tvw_hoten);
+        tvwMatKhau = findViewById(R.id.tvw_msv);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("Quynh", MODE_PRIVATE);
+        String strHoTen = sharedPreferences.getString("key_name", "");
+        tvwHoTen.setText(strHoTen);
     }
 }
