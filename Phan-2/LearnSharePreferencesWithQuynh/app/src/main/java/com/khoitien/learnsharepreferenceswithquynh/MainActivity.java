9882eb6ee776f37340fc8089b7217941a30e887a
Tạ Quang Khôi <khoi0941@gmail.com>
2022-10-26 08:15:37 +0700

    Thiết kế xong giao diện

--- Phan-2/LearnSharePreferencesWithQuynh/app/src/main/java/com/khoitien/learnsharepreferenceswithquynh/MainActivity.java
+++ Phan-2/LearnSharePreferencesWithQuynh/app/src/main/java/com/khoitien/learnsharepreferenceswithquynh/MainActivity.java
@@ -3,12 +3,25 @@ package com.khoitien.learnsharepreferenceswithquynh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtHoTen;
+    EditText edtMSV;
+    CheckBox cbxSaveInfo;
+    CheckBox btnDangNhap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AddControls();
+    }
+
+    private void AddControls() {
+        edtHoTen = findViewById(R.id.edt_hovaten);
+        edtMSV = findViewById(R.id.edt_msv);
     }
 }
