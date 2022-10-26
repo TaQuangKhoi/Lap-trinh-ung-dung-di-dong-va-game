fd23ed1144b43cbd1cf68f507c1346a04bcba275
Tạ Quang Khôi <khoi0941@gmail.com>
2022-10-26 10:42:15 +0700

    Xóa orj Text, Đổi ngôn ngữ ứng dụng

--- Phan-2/LearnSharePreferencesWithQuynh/app/src/main/java/com/khoitien/learnsharepreferenceswithquynh/MainActivity.java
+++ Phan-2/LearnSharePreferencesWithQuynh/app/src/main/java/com/khoitien/learnsharepreferenceswithquynh/MainActivity.java
@@ -1,17 +1,23 @@
 package com.khoitien.learnsharepreferenceswithquynh;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     EditText edtHoTen;
@@ -19,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     CheckBox cbxSaveInfo;
     Button btnDangNhap;
     TextView tvwDebug;
+    Button btnChangeLang;
 
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
@@ -29,13 +36,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(getResources().getString(R.string.app_name));
+
         AddSharedPreferences();
 
         AddControls();
         AddEvents();
 
-
         ReloadSavedInfo();
+
+        btnChangeLang.setOnClickListener(view -> ShowLanguageDialog());
+
     }
 
     private void AddControls() {
@@ -44,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         cbxSaveInfo = findViewById(R.id.cbx_save_info);
         btnDangNhap = findViewById(R.id.btn_dang_nhap);
         tvwDebug = findViewById(R.id.tvw_debug);
+        btnChangeLang = findViewById(R.id.btn_change_language);
     }
 
     private void AddEvents() {
@@ -60,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 editor.clear();
                 editor.putBoolean("key_bool", b).apply();
-                Toast.makeText(MainActivity.this, "Đã xóa", Toast.LENGTH_SHORT);
+                Toast.makeText(MainActivity.this, "Đã xóa", Toast.LENGTH_SHORT).show();
                 Log.i("SaveInfo", "Đã xóa");
             }
         });
@@ -78,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
         editor.putString("key_password", password).apply();
         editor.commit();
         tvwDebug.setText(name + password);
-        Toast.makeText(MainActivity.this, "Đã lưu", Toast.LENGTH_SHORT);
+        Toast.makeText(MainActivity.this, "Đã lưu", Toast.LENGTH_SHORT).show();
         Log.i("SaveInfo", "Đã chạy hàm");
     }
 
@@ -89,11 +103,73 @@ public class MainActivity extends AppCompatActivity {
             password = sharedPreferences.getString("key_password", "Password");
             edtHoTen.setText(name);
             edtMSV.setText(password);
-            Toast.makeText(MainActivity.this, "Đã khôi phục", Toast.LENGTH_SHORT);
+            Toast.makeText(MainActivity.this, "Đã khôi phục", Toast.LENGTH_SHORT).show();
         }
     }
 
     private void ShowLanguageDialog () {
-        final String[] listAvailableLanguage = {"","",""};
+        final String[] listAvailableLanguage = {"Tiếng Việt", "日本", "English"};
+
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+
+        mBuilder.setTitle("Choose Language...");
+        mBuilder.setSingleChoiceItems(listAvailableLanguage, -1,
+                (dialogInterface, i) -> {
+                    if(i == 0)
+                    {
+                        //Tiếng Việt
+                        setLocale("vi");
+                        Relauch();
+                    }
+                    else if(i == 1)
+                    {
+                        //Tiếng Trung
+                        setLocale("zh");
+                        Relauch();
+                    }
+                    else if(i == 2)
+                    {
+                        //Tiếng Anh
+                        setLocale("en");
+                        Relauch();
+                    }
+
+                    //tắt alert dialog khi đã chọn ngôn ngữ
+                    dialogInterface.dismiss();
+                });
+
+        AlertDialog mDialog = mBuilder.create();
+        //Hiện AlterDialog
+        mDialog.show();
+
     }
+
+    private void Relauch() {
+        finish();
+        startActivity(getIntent());
+        overridePendingTransition(0, 0); // chạy luôn, không cần xếp trong hàng đợi
+    }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(
+                config, getBaseContext().getResources().getDisplayMetrics());
+
+        //Lưu dữ liệu vào shared preferences
+
+        SharedPreferences.Editor editor = getSharedPreferences("Setting", MODE_PRIVATE).edit();
+        editor.putString("My_Lang", lang);
+        editor.apply();
+    }
+
+    //Đưa ngôn ngữ được lưu vào shared preferences
+    public void loadLocale(){
+        SharedPreferences prefs = getSharedPreferences("Setting", MODE_PRIVATE);
+        String language = prefs.getString("My_Lang", "");
+        setLocale(language);
+    }
+
 }
