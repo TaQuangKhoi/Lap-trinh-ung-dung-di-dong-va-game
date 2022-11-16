fd3fbf37618064ae8b21c96c5d2316b1b88af5e4
Tạ Quang Khôi <khoi0941@gmail.com>
2022-11-16 09:45:16 +0700

    Xong ORC

--- Phan-2/ORC/app/src/main/java/com/taquangkhoi/orc/MainActivity.java
+++ Phan-2/ORC/app/src/main/java/com/taquangkhoi/orc/MainActivity.java
@@ -2,18 +2,33 @@ package com.taquangkhoi.orc;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.SparseArray;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     Button btnTakePhoto, btnCopy, btnPickPhoto;
@@ -54,7 +69,11 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             cameraLauncher.launch(intent);
         });
-        btnPickPhoto.setOnClickListener(v -> {});
+        btnPickPhoto.setOnClickListener(v -> {
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .start(MainActivity.this);
+        });
         btnCopy.setOnClickListener(v -> {});
     }
 
@@ -65,7 +84,67 @@ public class MainActivity extends AppCompatActivity {
                 if (result.getResultCode() == RESULT_OK) {
                     Intent data = result.getData();
                     Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                    getTextFromImage(bitmap);
                 }
             }
     );
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
+                    getTextFromImage(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // ActivityResultLauncher for gallery
+    ActivityResultLauncher<Intent> galleryLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    Intent data = result.getData();
+                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                    // if bitmap has data
+                    if (bitmap != null) {
+                        getTextFromImage(bitmap);
+                    }
+                }
+            }
+    );
+
+    private void getTextFromImage(Bitmap bitmap) {
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        if (!textRecognizer.isOperational()) {
+            tvwResult.setText("Could not get the text");
+        } else {
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> items = textRecognizer.detect(frame);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < items.size(); i++) {
+                TextBlock myItem = items.valueAt(i);
+                sb.append(myItem.getValue());
+                sb.append("\n");
+            }
+            tvwResult.setText(sb.toString());
+            btnCopy.setVisibility(android.view.View.VISIBLE);
+            copyToClipboard();
+        }
+    }
+
+    private void copyToClipboard() {
+        ClipboardManager clipboard
+                = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("label", tvwResult.getText().toString());
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(this, "Copied to clipboard", Toast.LENGTH_SHORT).show();
+    }
 }
