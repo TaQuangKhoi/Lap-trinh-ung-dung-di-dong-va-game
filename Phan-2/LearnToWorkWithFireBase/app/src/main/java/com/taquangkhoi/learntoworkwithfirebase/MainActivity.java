4d2514b8525d4dc14f735ac2db3cbafd9498b7b7
Tạ Quang Khôi <khoi0941@gmail.com>
2022-11-23 09:06:25 +0700

    Xong Layout

---
+++ Phan-2/LearnToWorkWithFireBase/app/src/main/java/com/taquangkhoi/learntoworkwithfirebase/MainActivity.java
@@ -0,0 +1,57 @@
+package com.taquangkhoi.learntoworkwithfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference testRef = database.getReference("message");
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addControls();
+
+        testReadFromDB();
+
+    }
+
+    private void addControls() {
+    }
+
+    private void testWriteToDB() {
+        // Write a message to the database
+        testRef.setValue("Hello, World!");
+    }
+
+    private void testReadFromDB() {
+        // Read from the database
+        testRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
+}
