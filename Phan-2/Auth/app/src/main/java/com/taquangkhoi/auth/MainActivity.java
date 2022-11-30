665e1120870be1fe0686eda7dc0c322e232895ee
Tạ Quang Khôi <khoi0941@gmail.com>
2022-11-30 08:51:48 +0700

    Xong App Auth, chuẩn bị test

--- Phan-2/Auth/app/src/main/java/com/taquangkhoi/auth/MainActivity.java
+++ Phan-2/Auth/app/src/main/java/com/taquangkhoi/auth/MainActivity.java
@@ -1,14 +1,66 @@
 package com.taquangkhoi.auth;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSignInWithGoogle;
+    GoogleSignInOptions gso;
+    GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignInWithGoogle = findViewById(R.id.btnSignIn);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        btnSignInWithGoogle.setOnClickListener(v -> {
+            SignIn();
+        });
+    }
+
+    void SignIn() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 0) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                task.getResult(ApiException.class);
+                ProfileActivity();
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    void ProfileActivity() {
+        finish();
+        Intent intent = new Intent(this, SignOutActivity.class);
+        startActivity(intent);
     }
 }
--- Phan-2/Auth/app/src/main/java/com/taquangkhoi/auth/SignOutActivity.java
+++ Phan-2/Auth/app/src/main/java/com/taquangkhoi/auth/SignOutActivity.java
@@ -2,13 +2,54 @@ package com.taquangkhoi.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class SignOutActivity extends AppCompatActivity {
+    TextView tvwName, tvwAge;
+    Button btnSignOut, btnClose;
+    GoogleSignInOptions gso;
+    GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_out);
+
+        tvwName = findViewById(R.id.tvwName);
+        tvwAge = findViewById(R.id.tvwAge);
+
+        btnClose = findViewById(R.id.btnClose);
+        btnSignOut = findViewById(R.id.btnSignOut);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null) {
+            tvwName.setText(account.getDisplayName());
+            tvwAge.setText(account.getEmail());
+        }
+
+        btnSignOut.setOnClickListener(v -> {
+            googleSignInClient.signOut();
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        });
+
+        btnClose.setOnClickListener(v -> {
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        });
     }
 }
