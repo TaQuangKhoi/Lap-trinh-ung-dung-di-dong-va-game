68019a070ad68fc725829123f9cd5690c81b8419
Tạ Quang Khôi <khoi0941@gmail.com>
2022-12-07 08:00:13 +0700

    feat: create 'INSERT DATA.sql'

---
+++ Phan-2/LearnPHP/INSERT DATA.sql
@@ -0,0 +1,17 @@
+INSERT INTO
+    `furniture` (`id`, `name`, `detail`, `price`, `image`)
+VALUES
+    (
+        NULL,
+        'ghế',
+        'ghế để ngồi',
+        '20.5',
+        'http://10.0.2.2/LearnPhpMySqlApp/img/f1.png'
+    ),
+    (
+        NULL,
+        'đèn',
+        'đèn ngủ với em',
+        '500',
+        'http://10.0.2.2/LearnPhpMySqlApp/img/f2.png'
+    )
